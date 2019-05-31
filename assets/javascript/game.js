cd3232de0c4781e96365385b1886d96190b068b0
Ryan Ward <ryanward4556@gmail.com>
2019-05-31 12:42:48 -0400

    set up psuedocode in js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+
+// Display the game "Psychic Game...Guess what letter I'm thinking of"
+
+
+// Listens for the user to guess
+
+document.onkeyup = function(event) {
+
+
+// Set the comp's guess
+
+
+// choose a random letter out of full alphabet 
+
+
+// Once the user guesses, record their guess letter
+
+
+// Subtract 1 Guess Left. 
+
+
+// Display user guess on "Your guesses so far: "
+
+
+// Compare user guess to comp guess
+
+
+// If they're the same record a W and restart the game
+
+
+// If not keep listening for guesses until user guesses so far is 0
+
+
+// If user guesses so far reaches 0, record an L and restart the game
+
